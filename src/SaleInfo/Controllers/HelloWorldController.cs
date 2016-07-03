eea6b3bc7a8b0fd69ce46457d51c6d6ea02e34a4
Tolyan91 <klaviat91@mail.ru>
2016-07-04 02:08:30 +0600

    тест

--- src/SaleInfo/Controllers/HelloWorldController.cs
+++ src/SaleInfo/Controllers/HelloWorldController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace SaleInfo.Controllers
 {
-    public class IndexController : Controller
+    public class HelloWorldController : Controller
     {
+        
         // GET: /<controller>/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is IndexController!";
+            return View();
         }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+        
     }
 }
